40123874b50a8ec1017167ca0b05f6ad36f4a2ca
Raymond Michael Nziku <nziku99@gmail.com>
2023-02-09 17:38:16 +0300

    initializing UI

---
+++ src/client/lib/types.ts
@@ -0,0 +1,11 @@
+export type IUser = {
+    _id: string;
+    id: string;
+    email: string;
+    name: string;
+    role: string;
+    photo: string;
+    updatedAt: string;
+    createdAt: string;
+};
+
