0bcccb20aaef2927c90c31317aa7bbf2ec06a350
Raymond Michael Nziku <nziku99@gmail.com>
2023-02-10 08:25:38 +0300

    added utility fns for cleaning the response also hashing & verifying passwords

---
+++ src/server/utils/pass.ts
@@ -0,0 +1,14 @@
+import { compare, genSalt, hash } from "bcryptjs";
+
+
+export async function hashPassword(password: string): Promise<string> {
+    const salt = await genSalt(12);
+    return hash(password, salt);
+}
+
+export function comparePasswords(
+    candidatePassword: string,
+    hashedPassword: string,
+): Promise<boolean> {
+    return compare(candidatePassword, hashedPassword);
+}
